67fe499c417136b6450dd990a4b7ef305e26805e
Akash Das <Aku.das02@gmail.com>
2022-01-19 01:41:57 +0600

    refactor: create a common component named Section Header for all sections

---
+++ API/elements/sectionHeaders.js
@@ -0,0 +1,32 @@
+export const SectionHeaders = [
+    {
+        id: 1,
+        title: "what I do",
+        subtitle: "features",
+        align: "left"
+    },
+    {
+        id: 2,
+        title: "My Portfolio",
+        subtitle: "Visit my portfolio and keep your feedback",
+        align: "center"
+    },
+    {
+        id: 3,
+        title: "My Resume",
+        subtitle: "7+ Years of Experience",
+        align: "center"
+    },
+    {
+        id: 4,
+        title: "My Blog",
+        subtitle: "Visit my blog and keep your feedback",
+        align: "center"
+    },
+    {
+        id: 5,
+        title: "Contact With Me",
+        subtitle: "contact",
+        align: "center"
+    }
+]
---
+++ src/components/common/sectionHeaders/style.js
@@ -0,0 +1,38 @@
+import {makeStyles} from "@material-ui/core/styles";
+
+import FONTS from "../../../../styles/fonts";
+
+const useStyles = makeStyles({
+    sectionHeaderWrapper: theme => ({
+        '&__subtitle': {
+            display: "block",
+            color: theme.palette.customColor.highContrast,
+            fontSize: 14,
+            fontFamily: FONTS.primaryFont,
+            fontWeight: 500,
+            letterSpacing: 1,
+            lineHeight: "14px",
+            textTransform: "uppercase",
+            marginBottom: 10
+        },
+        '&__title': {
+            color: theme.palette.customColor.main,
+            fontSize: 60,
+            fontFamily: FONTS.primaryFont,
+            fontWeight: 700,
+            lineHeight: 1.2,
+            textTransform: "capitalize",
+            marginTop: 15,
+        },
+        '&--left': {
+            textAlign: "left"
+        },
+        '&--center': {
+            textAlign: "center"
+        }
+    })
+});
+
+export function SectionHeaderStyle(theme) {
+    return useStyles(theme);
+}
