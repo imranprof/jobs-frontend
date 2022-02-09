2e7ca874fe09482c42b5d6d6bfb50813f957c616
Kamrul Hassan <kh27.abir@gmail.com>
2022-02-09 20:34:33 +0600

    feat: portfolio modal
    - create customModal component
    - render modal onClick portfolio card

--- src/components/CustomMui/Card/card.js
+++ src/components/CustomMui/Card/card.js
@@ -1,12 +1,13 @@
 import Link from 'next/link'
 
-import {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import {Card, CardContent, CardMedia} from "@material-ui/core";
 import Icon from "@material-ui/core/Icon";
 
 import ThemeContextProvider from "../../../contexts/themeContext";
 import FontAwesomeIcons from "../../../../styles/FontAwesomeIcons";
 import {CardStyle} from "./style";
+import CustomModal from "../../Portfolio/customModal";
 
 const CustomCard = ({element, elementType}) => {
     const customTheme = useContext(ThemeContextProvider);
@@ -16,38 +17,51 @@ const CustomCard = ({element, elementType}) => {
     const iconClass = isPortfolio ? "heart" : "clock";
     const elementData = isPortfolio ? element.reactCount : `${element.readTime} read`;
 
+    const [toggleModal, setToggleModal] = useState(false);
+
     return (
-        <Card xs={12} sm={6} md={4} className={classes.cardWrapper}>
-            <div className={`${classes.cardWrapper}__image-wrapper`}>
-                <CardMedia
-                    className={`${classes.cardWrapper}__image`}
-                    image={element.image}
-                    title={element.title}
-                />
-            </div>
-            <CardContent className={`${classes.cardWrapper}__content`}>
-                <div className={`${classes.cardWrapper}__category-info`}>
-                    <div className={`${classes.cardWrapper}__category-info__category`}>
-                        <Link href="/">
-                            <a>{element.category}</a>
-                        </Link>
-                    </div>
-                    <div className={`${classes.cardWrapper}__category-info__icon-wrapper`}>
-                        <Icon
-                            className={`${classes.cardWrapper}__category-info__react ${FontAwesomeIcons[iconClass]}`}/>
-                        {elementData}
-                    </div>
+        <>
+            <Card xs={12} sm={6} md={4}
+                  className={classes.cardWrapper}
+                  onClick={() => {
+                      setToggleModal(true)
+                  }}>
+                <div className={`${classes.cardWrapper}__image-wrapper`}>
+                    <CardMedia
+                        className={`${classes.cardWrapper}__image`}
+                        image={element.image}
+                        title={element.title}
+                    />
                 </div>
-                <h1 className={`${classes.cardWrapper}__title`}>
-                    <Link href="/" >
-                        <a className={`${classes.cardWrapper}__title__link`}>
-                            {element.title}
-                            <Icon className={`${classes.cardWrapper}__title__link__arrow ${FontAwesomeIcons.arrow}`}/>
-                        </a>
-                    </Link>
-                </h1>
-            </CardContent>
-        </Card>
+                <CardContent className={`${classes.cardWrapper}__content`}>
+                    <div className={`${classes.cardWrapper}__category-info`}>
+                        <div className={`${classes.cardWrapper}__category-info__category`}>
+                            <Link href="/">
+                                <a>{element.category}</a>
+                            </Link>
+                        </div>
+                        <div className={`${classes.cardWrapper}__category-info__icon-wrapper`}>
+                            <Icon
+                                className={`${classes.cardWrapper}__category-info__react ${FontAwesomeIcons[iconClass]}`}/>
+                            {elementData}
+                        </div>
+                    </div>
+                    <h1 className={`${classes.cardWrapper}__title`}>
+                        <Link href="/">
+                            <a className={`${classes.cardWrapper}__title__link`}>
+                                {element.title}
+                                <Icon
+                                    className={`${classes.cardWrapper}__title__link__arrow ${FontAwesomeIcons.arrow}`}/>
+                            </a>
+                        </Link>
+                    </h1>
+                </CardContent>
+            </Card>
+            {toggleModal && <CustomModal
+                setToggleModal={setToggleModal}
+                portfolio={element}/>
+            }
+        </>
     );
 }
 
---
+++ src/components/Portfolio/customModal.js
@@ -0,0 +1,74 @@
+import React, {useContext, useState} from 'react';
+
+import {Grid, IconButton} from "@material-ui/core";
+import Icon from "@material-ui/core/Icon";
+import CloseIcon from "@material-ui/icons/Close";
+
+import ThemeContextProvider from "../../contexts/themeContext";
+import FontAwesomeIcons from "../../../styles/FontAwesomeIcons";
+import {ModalStyle} from "./modalStyle";
+
+const CustomModal = ({
+                         setToggleModal,
+                         portfolio: {image, category, title, description}
+                     }) => {
+    const customTheme = useContext(ThemeContextProvider);
+    const modalWrapper = ModalStyle(customTheme).modalWrapper;
+    const [slidingClass, SetSlidingClass] = useState("");
+
+    setTimeout(() => {
+        SetSlidingClass(setToggleModal ? `${modalWrapper}__modal-content--visible` : "")
+    }, 1);
+
+    return (
+        <div className={`${modalWrapper}__body`}>
+            <div className={`${modalWrapper}__dialog`}>
+                <div className={`${modalWrapper}__modal-content ${slidingClass}`} >
+                    <IconButton className={`${modalWrapper}__modal-content__close-icon`}
+                                onClick={() => {
+                                    setToggleModal(false)
+                                }}>
+                        <CloseIcon/>
+                    </IconButton>
+                    <Grid container>
+                        <Grid item md={6} className={`${modalWrapper}__modal-content__image-container`}>
+                            <img
+                                className={`${modalWrapper}__modal-content__image-container__image`}
+                                src={image}
+                                alt="image"
+                            />
+                        </Grid>
+                        <Grid item md={6} className={`${modalWrapper}__modal-content__text-content`}>
+                                <p className={`${modalWrapper}__modal-content__text-content__category`}>
+                                    {category}
+                                </p>
+                                <p className={`${modalWrapper}__modal-content__text-content__title`}>
+                                    {title}
+                                </p>
+                                <p className={`${modalWrapper}__modal-content__text-content__description`}>
+                                    {description}
+                                </p>
+                                <div
+                                    className={`${modalWrapper}__modal-content__text-content__link-button-wrapper`}>
+                                    <a
+                                        className={`${modalWrapper}__modal-content__text-content__link-button-wrapper__link-button`}>
+                                        LIKE THIS
+                                        <Icon
+                                            className={`${FontAwesomeIcons.thumbsUp} ${modalWrapper}__modal-content__text-content__link-button-wrapper__link-button__thumbs-up-icon`}/>
+                                    </a>
+                                    <a
+                                        className={`${modalWrapper}__modal-content__text-content__link-button-wrapper__link-button`}>
+                                        VIEW PROJECT
+                                        <Icon
+                                            className={`${FontAwesomeIcons.angleRight} ${modalWrapper}__modal-content__text-content__link-button-wrapper__link-button__right-angular-icon`}/>
+                                    </a>
+                                </div>
+                        </Grid>
+                    </Grid>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default CustomModal;
