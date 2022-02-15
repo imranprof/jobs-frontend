4a9b08512d92aae2c6bf2a5a3222781be5a60999
Akash Das <aku.das02@gmail.com>
2022-02-16 05:06:27 +0600

    refactor: add Next Head component in _app.js file and add padding x-axis in mobile devices

--- src/components/Home/style.js
+++ src/components/Home/style.js
@@ -7,7 +7,6 @@ export const useStyles = makeStyles({
         width: "100%",
         display: "flex",
         flexWrap: "wrap-reverse",
-
         // homeWrapper left side
         '&__left': {
             display: "flex",
@@ -131,6 +130,9 @@ export const useStyles = makeStyles({
                 marginBottom: 50
             }
         },
+        [theme.breakpoints.down('xs')]: {
+            padding: "0px 16px"
+        }
     })
 });
 
