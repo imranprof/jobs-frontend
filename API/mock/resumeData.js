4cac654381adb9da1d032ae4bca044f721bcbd54
Kamrul Hassan <kh27.abir@gmail.com>
2022-02-24 16:53:05 +0600

    misc: update resume mock data

--- API/mock/resumeData.js
+++ API/mock/resumeData.js
@@ -49,40 +49,40 @@ export const ResumeData = {
         {id: 1, name: "ruby", rating: 100},
         {id: 2, name: "javascript", rating: 73},
         {id: 3, name: "photoshop", rating: 90},
-        {id: 4, name: "figma", rating: 15},
+        {id: 4, name: "figma", rating: 65},
         {id: 5, name: "adobe", rating: 56},
     ],
     experience: [
         {
             id: 1,
-            title: "University of A",
+            title: "Diploma in Web Development",
             startDate: "Jan 2018",
             endDate: "Jan 2022",
-            description: "The education should be very interactual. Ut tincidunt est ac dolor aliquam sodales. Phasellus sed mauris hendrerit, laoreet sem in, lobortis mauris hendrerit ante.",
+            description: "Contrary to popular belief. Ut tincidunt est ac dolor aliquam sodales. Phasellus sed mauris hendrerit, laoreet sem in, lobortis mauris hendrerit ante.",
             rating: ""
         },
         {
             id: 2,
-            title: "University of B",
+            title: "The Personal Portfolio Mystery",
             startDate: "Jan 2018",
             endDate: "Jan 2022",
-            description: "The education should be very interactual. Ut tincidunt est ac dolor aliquam sodales. Phasellus sed mauris hendrerit, laoreet sem in, lobortis mauris hendrerit ante.",
+            description: "Contrary to popular belief. Ut tincidunt est ac dolor aliquam sodales. Phasellus sed mauris hendrerit, laoreet sem in.",
             rating: ""
         },
         {
             id: 3,
-            title: "University of C",
+            title: "Diploma in Computer Science",
             startDate: "Jan 2018",
             endDate: "Jan 2022",
-            description: "The education should be very interactual. Ut tincidunt est ac dolor aliquam sodales. Phasellus sed mauris hendrerit, laoreet sem in, lobortis mauris hendrerit ante.",
+            description: "Ut tincidunt est ac dolor aliquam sodales. Phasellus sed mauris hendrerit, laoreet sem in, lobortis mauris hendrerit ante.",
             rating: ""
         },
         {
             id: 4,
-            title: "University of D",
+            title: "Tips For Personal Portfolio",
             startDate: "Jan 2018",
             endDate: "Jan 2022",
-            description: "The education should be very interactual. Ut tincidunt est ac dolor aliquam sodales. Phasellus sed mauris hendrerit, laoreet sem in, lobortis mauris hendrerit ante.",
+            description: "If you are going to use a passage. Ut tincidunt est ac dolor aliquam sodales. laoreet sem in, lobortis mauris hendrerit ante.",
             rating: ""
         }
     ],
