886ec74954f98fb5bd7b6c33cd9fddb808aee854
Kamrul Hassan <kh27.abir@gmail.com>
2022-02-25 18:19:38 +0600

    refactor: remove unnecessary skills item check

--- src/components/Resume/Elements/contentItem.js
+++ src/components/Resume/Elements/contentItem.js
@@ -1,35 +1,29 @@
-import {Grid} from "@material-ui/core";
 import {useContext} from "react";
+
+import {Grid} from "@material-ui/core";
+
 import ThemeContextProvider from "../../../contexts/themeContext";
 import {ResumeStyle} from "../style";
 
 const ContentItem = ({cardType, cardContent}) => {
     const customTheme = useContext(ThemeContextProvider);
     const resumeWrapper = ResumeStyle(customTheme).resumeWrapper;
+    const isEducationContent = () => cardType === "education";
 
-    const title = () => {
-        if (cardType === "education") return cardContent.institution
-        else if (cardType === "experience") return cardContent.title
-    }
-    const subTitle = () => {
-        if (cardType !== "skills") return `${cardContent.startDate} - ${cardContent.endDate}`
-    }
-    const description = () => {
-        if (cardType !== "skills") return cardContent.description
-    }
+    const title = () => isEducationContent() ? cardContent.institution : cardContent.title;
+    const subTitle = () => `${cardContent.startDate} - ${cardContent.endDate}`;
+    const description = () => cardContent.description;
+
     return (
-        <Grid item container
-              className={`${resumeWrapper}__nav-content__row__column__content__item`}>
+        <Grid item container className={`${resumeWrapper}__nav-content__row__column__content__item`}>
             <div className={`${resumeWrapper}__nav-content__row__column__content__item__inner`}>
                 <Grid className={`${resumeWrapper}__nav-content__row__column__content__item__inner__heading`}>
-                    <div
-                        className={`${resumeWrapper}__nav-content__row__column__content__item__inner__heading__title`}>
+                    <div className={`${resumeWrapper}__nav-content__row__column__content__item__inner__heading__title`}>
                         <h4>{title()}</h4>
                         <span>{subTitle()}</span>
                     </div>
                 </Grid>
-                <Grid
-                    className={`${resumeWrapper}__nav-content__row__column__content__item__inner__description`}>
+                <Grid className={`${resumeWrapper}__nav-content__row__column__content__item__inner__description`}>
                     <p>{description()}</p>
                 </Grid>
             </div>
