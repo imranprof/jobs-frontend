4538f66a327c64d2fb46edd64225ec641d48b7ab
Kamrul Hassan <kh27.abir@gmail.com>
2022-02-25 18:46:32 +0600

    refactor: make columnContent function as component

--- src/components/Resume/Elements/rowContent.js
+++ src/components/Resume/Elements/rowContent.js
@@ -1,11 +1,10 @@
 import {useContext} from 'react';
-import ThemeContextProvider from "../../../contexts/themeContext";
 
 import {Grid} from "@material-ui/core";
 
-import SkillsItem from "./skillsItem";
-import ContentItem from "./contentItem";
+import ThemeContextProvider from "../../../contexts/themeContext";
 import {ResumeStyle} from "../style";
+import ColumnContent from "./columnContent";
 
 const RowContent = ({cardType, cardData}) => {
     const customTheme = useContext(ThemeContextProvider);
@@ -16,28 +15,10 @@ const RowContent = ({cardType, cardData}) => {
         return (
             <Grid key={position} item xs={6} className={`${resumeWrapper}__nav-content__row__column`}>
                 <Grid container className={`${resumeWrapper}__nav-content__row__column__${wrapperClass}`}>
-                    {columnContent(position)}
+                    <ColumnContent position={position} cardType={cardType} cardData={cardData} />
                 </Grid>
             </Grid>
         );
-    }
-
-    const columnContent = position => {
-        const isLeftColumn = () => position === "left";
-
-        const itemCount = cardData.length;
-        const columnLimit = Math.floor((itemCount + 1) / 2);
-        const startIndex = isLeftColumn() ? 0 : columnLimit;
-        const endIndex = isLeftColumn() ? columnLimit : itemCount;
-
-        let content = [];
-        for (let idx = startIndex; idx < endIndex; idx++) {
-            content.push(
-                cardType === "skills" ? <SkillsItem key={cardData[idx].id} cardType={cardType} cardContent={cardData[idx]}/> :
-                    <ContentItem key={cardData[idx].id} cardType={cardType} cardContent={cardData[idx]}/>
-            );
-        }
-        return content;
     }
 
     return (
