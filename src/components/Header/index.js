22c255fdaaaaf15ee2c5f6ad9896b725f37f7d53
Kamrul Hassan <kh27.abir@gmail.com>
2022-03-09 18:14:18 +0600

    feat: temporary necessary buttons

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect, useRef} from 'react';
+import React, {useContext, useEffect, useRef, useState} from 'react';
 
 import {AppBar, Hidden, Toolbar,} from "@material-ui/core";
 
@@ -8,13 +8,18 @@ import ProfileInfo from "./profileInfo";
 import SideBar from "./sideBar";
 import NavItems from "./navItems";
 import {HeaderStyle} from './style';
+import Button from "@material-ui/core/Button";
+import SignInModal from "../../auth/SignInModal";
+import {SignOut} from "../../auth/operations";
+import SignUpModal from "../../auth/SignUpModal";
 
 const Header = () => {
     const customTheme = useContext(ThemeContextProvider);
     const classes = HeaderStyle(customTheme);
     const {name, avatar} = profileData;
     const headerRef = useRef(null);
-
+    const [showSignUpModal, setShowSignUpModal] = useState(false);
+    const [showSignInModal, setShowSignInModal] = useState(false);
     useEffect(() => {
         window.addEventListener('scroll', isSticky);
         return () => {
@@ -31,7 +36,15 @@ const Header = () => {
     return (
         <AppBar className={classes.headerWrapper} ref={headerRef}>
             <Toolbar className={`${classes.headerWrapper}__toolbar`}>
-                <ProfileInfo name={name} avatar={avatar} showName={true} classes={classes} />
+                <ProfileInfo name={name} avatar={avatar} showName={true} classes={classes}/>
+
+                <Button onClick={() => setShowSignUpModal(true)}>Sign Up</Button>
+                {showSignUpModal && <SignUpModal setShowSignUpModal={setShowSignUpModal}/>}
+
+                <Button onClick={() => setShowSignInModal(true)}>Sign In</Button>
+                {showSignInModal && <SignInModal setShowSignInModal={setShowSignInModal}/>}
+
+                <Button onClick={SignOut}>Sign Out</Button>
                 <SideBar name={name} avatar={avatar} classes={classes}/>
                 <Hidden mdDown>
                     <NavItems classes={classes} variant={"default"}/>
