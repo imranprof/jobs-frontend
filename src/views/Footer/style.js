6ea9183335ea4fcf23576101dc2fe4941ac544e1
Akash Das <aku.das02@gmail.com>
2022-03-14 19:34:50 +0600

    style: set footer position at bottom of screen
    - use minimum height in MUI container component

--- src/views/Footer/style.js
+++ src/views/Footer/style.js
@@ -8,7 +8,7 @@ export const useStyles = makeStyles({
         flexDirection: "column",
         justifyContent: "center",
         alignItems: "center",
-        padding: '26px 56px',
+        padding: '50px 56px 10px 56px',
         '&__logo': {
             width: 60,
             height: 60,
--- src/views/Layout/index.js
+++ src/views/Layout/index.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 
 import {ThemeProvider} from "@material-ui/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
@@ -9,11 +9,15 @@ import darkTheme from "../../../styles/darkTheme";
 import lightTheme from "../../../styles/lightTheme";
 import Header from "../Header"
 import Footer from "../Footer";
+import ThemeContextProvider from "../../contexts/themeContext";
+import {LayoutStyle} from "./style";
 
 function withLayout(Component, type) {
   return (props) => {
     const [darkMode, setDarkMode] = useState(true)
     const [customTheme, setCustomTheme] = useState(darkTheme)
+    const customThemeProvider = useContext(ThemeContextProvider);
+    const classes = LayoutStyle(customThemeProvider);
 
     useEffect(() => {
       setCustomTheme(darkMode ? darkTheme : lightTheme)
@@ -24,7 +28,7 @@ function withLayout(Component, type) {
           <ThemeProvider theme={{...customTheme}}>
             <CssBaseline/>
             <Header type={type}/>
-            <Container fixed>
+            <Container fixed className={`${classes.layoutWrapper}__container`}>
               <Component {...props} />
             </Container>
             <Footer/>
