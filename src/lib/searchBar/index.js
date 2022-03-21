0e05cbafff1896bc360a0d2c930a23107479542c
Akash Das <aku.das02@gmail.com>
2022-03-21 19:37:12 +0600

    refactor: resolve review changes

--- src/lib/searchBar/index.js
+++ src/lib/searchBar/index.js
@@ -9,32 +9,34 @@ import ThemeContextProvider from "../../contexts/themeContext";
 import {SearchBarStyle} from "./style";
 
 const SearchBar = () => {
-  const customTheme = useContext(ThemeContextProvider);
-  const classes = SearchBarStyle(customTheme);
+    const customTheme = useContext(ThemeContextProvider);
+    const classes = SearchBarStyle(customTheme);
 
-  const formik = useFormik({
-    initialValues: {searchValue: ''},
-    onSubmit: values => {}
-  })
+    const formik = useFormik({
+        initialValues: {searchValue: ''},
+        onSubmit: values => {
+        }
+    })
 
-  return (
-    <form onSubmit={formik.handleSubmit}>
-      <InputBase name="searchValue"
-                 type="text"
-                 placeholder="Search..."
-                 value={formik.values.searchValue}
-                 onChange={formik.handleChange}
-                 className={`${classes.searchBarWrapper}__search`}
-                 endAdornment={
-                   <InputAdornment position="end">
-                     <button type="submit" className={`${classes.searchBarWrapper}__search__icon`}>
-                       <SearchIcon />
-                     </button>
-                   </InputAdornment>
-                 }
-      />
-    </form>
-  );
+    return (
+        <form onSubmit={formik.handleSubmit}>
+            <InputBase
+                name="searchValue"
+                type="text"
+                placeholder="Search..."
+                value={formik.values.searchValue}
+                onChange={formik.handleChange}
+                className={`${classes.searchBarWrapper}__search`}
+                endAdornment={
+                    <InputAdornment position="end">
+                        <button type="submit" className={`${classes.searchBarWrapper}__search__icon`}>
+                            <SearchIcon/>
+                        </button>
+                    </InputAdornment>
+                }
+            />
+        </form>
+    );
 };
 
 export default SearchBar;
--- src/pages/profiles/index.js
+++ src/pages/profiles/index.js
@@ -1,25 +1,19 @@
-import {useState} from "react";
+import {useContext, useState} from "react";
 import InfiniteScroll from "react-infinite-scroll-component";
 
 import {Grid} from "@material-ui/core";
-import { makeStyles } from '@material-ui/core/styles';
 
 import {ProfileCardData} from "../../../API/mock/profiles/profileCardData";
 import withLayout from "../../views/Layout";
 import ProfileCard from "../../views/Profiles/ProfileCard";
 import SectionHeader from "../../lib/sectionHeader";
 import CustomLoader from "../../lib/customLoader";
-
-const useStyles = makeStyles({
-  profilesWrapper: {
-    display: "flex",
-    flexWrap: "wrap",
-    justifyContent: "space-around"
-  },
-});
+import ThemeContextProvider from "../../contexts/themeContext";
+import {ProfilesStyle} from "./style";
 
 const Profiles = () => {
-  const classes = useStyles();
+  const customTheme = useContext(ThemeContextProvider);
+  const classes = ProfilesStyle(customTheme);
   const itemLength = ProfileCardData.length
   const chunkedData = ProfileCardData.slice(0, 8)
   const [items, setItems] = useState(chunkedData)
