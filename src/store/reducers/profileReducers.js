fc467e1d7cb8313d02c349b6cdd820435756dca6
Akash Das <aku.das02@gmail.com>
2022-04-12 14:49:21 +0600

    feat: implement redux for features in profile page

--- src/store/reducers/profileReducers.js
+++ src/store/reducers/profileReducers.js
@@ -17,6 +17,44 @@ const initialState = {
       {id: 1, image: "ruby.png", alt: "ruby"},
       {id: 2, image: "javascript.png", alt: "javascript"},
       {id: 3, image: "python.png", alt: "python"},
+    ],
+    features: [
+      {
+        id: 1,
+        iconName: "bars",
+        title: "business strategy",
+        description: "I throw myself down among the tall grass by the stream as I lie close to the earth."
+      },
+      {
+        id: 2,
+        iconName: "bookOpen",
+        title: "app development",
+        description: " It uses a dictionary of over 200 Latin words, combined with a handful of model sentence."
+      },
+      {
+        id: 3,
+        iconName: "tv",
+        title: "app design",
+        description: "I throw myself down among the tall grass by the stream as I lie close to the earth."
+      },
+      {
+        id: 4,
+        iconName: "tools",
+        title: "mobile app",
+        description: "There are many variations of passages of Lorem Ipsum available, but the majority."
+      },
+      {
+        id: 5,
+        iconName: "cogs",
+        title: "CEO marketing",
+        description: "always free from repetition, injected humour, or non-characteristic words etc."
+      },
+      {
+        id: 6,
+        iconName: "slack",
+        title: "UI & UX design",
+        description: "It uses a dictionary of over 200 Latin words, combined with a handful of model sentence."
+      },
     ]
   }
 }
