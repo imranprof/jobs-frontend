d13f7254d29589021fea066597654320d4ad9a06
imranprof <imran.ent320@gmail.com>
2022-06-03 21:13:26 +0600

    fix: modify popover style

--- src/lib/profile/profileshare/profileShare.js
+++ src/lib/profile/profileshare/profileShare.js
@@ -15,22 +15,28 @@ import {
 export const ProfileShare = (props) => {
     return (
 
-        <Box marginTop={.5} marginRight={.5} marginLeft={.5}>
+        <Box sx={{ display: 'flex', alignItems: 'center', margin: '5px'}}>
+            <div>
             <TwitterShareButton url={props.shareUrl}
                                 title={"Stay connect with me in Seek Right jobs\n\n"}
                                 tags="#seekRightjobs" outline-ofset="5">
                 <TwitterIcon borderRadius={15} size={30}/>
             </TwitterShareButton>
+            </div>
+            <Box marginLeft={1.5} marginRight={1.5}>
             <FacebookShareButton url={props.shareUrl}
                                  quote={"Stay connect with me in Seek Right jobs"}
                                  hashtag="#seekRightsjob">
-                <Box marginLeft={1.5} marginRight={1.5}><FacebookIcon borderRadius={15} size={30}/></Box>
+                <FacebookIcon borderRadius={15} size={30}/>
             </FacebookShareButton>
+            </Box>
+            <div>
             <LinkedinShareButton url={props.shareUrl}
                                  title={"Stay connect with me in Seek Right jobs"}
                                  source="www.seekrightjobs.org">
                 <LinkedinIcon  borderRadius={15} size={30}/>
             </LinkedinShareButton>
+            </div>
         </Box>
 
 
