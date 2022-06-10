d535f7392d640cfb41214ed215f0d6611f562ae1
imranprof <imran.ent320@gmail.com>
2022-06-10 16:46:08 +0600

    fix: remove extra line and indent it

--- src/lib/profile/profileshare/shareButton.js
+++ src/lib/profile/profileshare/shareButton.js
@@ -2,32 +2,28 @@ import React from "react";
 import {Button, ClickAwayListener} from "@material-ui/core";
 import ShareBar from "./shareBar";
 
-
 const ShareButton = ({classes}) => {
-    const [anchorEl, setAnchorEl] = React.useState(null);
-    const [openEl, setOpen] = React.useState(false);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [openEl, setOpen] = React.useState(false);
+  const handleClick = (event) => {
+    setAnchorEl(anchorEl ? null : event.currentTarget);
+    setOpen(openEl ? null : event.currentTarget)
+  };
+  const handleClose = () => {
+    setOpen(false);
+    setAnchorEl(null);
+  };
 
-
-    const handleClick = (event) => {
-        setAnchorEl(anchorEl ? null : event.currentTarget);
-        setOpen(openEl ? null : event.currentTarget)
-    };
-    const handleClose = () => {
-        setOpen(false);
-        setAnchorEl(null);
-    };
-    return (
-        <div>
-            <ClickAwayListener onClickAway={handleClose}>
-                <Button type="button" onClick={handleClick} className={`${classes.headerWrapper}__nav__share`}>
-                    Share
-                </Button>
-            </ClickAwayListener>
-            <ShareBar classes={classes} openEl={openEl} anchorEl={anchorEl}/>
-        </div>
-
-
-    );
+  return (
+    <div>
+      <ClickAwayListener onClickAway={handleClose}>
+        <Button type="button" onClick={handleClick} className={`${classes.headerWrapper}__nav__share`}>
+          Share
+        </Button>
+      </ClickAwayListener>
+      <ShareBar classes={classes} openEl={openEl} anchorEl={anchorEl}/>
+    </div>
+  );
 };
 
 export default ShareButton;
