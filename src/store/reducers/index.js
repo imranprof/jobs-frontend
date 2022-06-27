8ebe4ca5791f09de8d2e03b325fe493e66df76dd
imranprof <imran.ent320@gmail.com>
2022-06-27 20:30:01 +0600

    feat: combine blogReducers to rootReducer

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -3,12 +3,14 @@ import {profilesReducer} from "./profilesReducers";
 import {profileReducer} from "./profileReducers";
 import {authReducers} from "./authReducers";
 import {editProfileReducer} from "./editProfileReducers";
+import {blogReducer} from "./blogReducers";
 
 const rootReducers = combineReducers({
     allProfiles: profilesReducer,
     profile: profileReducer,
     editProfile: editProfileReducer,
-    auth: authReducers
+    auth: authReducers,
+    blogs: blogReducer
 })
 
 export default rootReducers;
