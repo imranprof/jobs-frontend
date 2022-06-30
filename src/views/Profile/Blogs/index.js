97132d2d5090445d3e67387e29fd2724041e0231
imranprof <imran.ent320@gmail.com>
2022-06-30 17:03:37 +0600

    refactor: remove unnecessary code

--- src/views/Profile/Blogs/index.js
+++ src/views/Profile/Blogs/index.js
@@ -5,28 +5,17 @@ import {Grid} from "@material-ui/core";
 import {useTheme} from "@material-ui/core/styles";
 
 import {BlogsStyle} from "./style";
-import RemoveButton from "../../../lib/removeButton";
-import {blogsRemove} from "../../../store/actions/blogActions";
 
 const Blogs = (props) => {
     const theme = useTheme();
     const classes = BlogsStyle(theme);
     const { blogs} = props;
 
-    const blogRemoveHandler =(blog_id)=> {
-        const currentBlogs = blogs.filter(blog=> blog.id !== blog_id)
-        props.blogsRemove(currentBlogs);
-    }
 
     return (
         <Grid container spacing={4} className={classes.blogsWrapper} id="blog">
             {blogs.map(blog => (
               <div key = {blog.id} className={`${classes.blogsWrapper}__blog-card`}>
-                  <div className={`${classes.blogsWrapper}__remove-button`}>
-                      <span onClick ={()=>blogRemoveHandler(blog.id)}>
-                         <RemoveButton />
-                      </span>
-                  </div>
                   <CustomCard key={blog.id} element={blog} elementType="blog"/>
               </div>
             ))}
@@ -40,8 +29,4 @@ const mapStateToProps = (state) => {
     }
 }
 
-const mapDispatchToProps = (dispatch) => ({
-    blogsRemove: (blogs) => dispatch(blogsRemove(blogs))
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(Blogs);
+export default connect(mapStateToProps)(Blogs);
