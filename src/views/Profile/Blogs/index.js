46efc6a7bd547732c1f1dd501eb718de87fbc728
imranprof <imran.ent320@gmail.com>
2022-07-04 20:10:25 +0600

    refactor: remove extra line

--- src/views/Profile/Blogs/index.js
+++ src/views/Profile/Blogs/index.js
@@ -14,7 +14,6 @@ const Blogs = (props) => {
     const { blogs} = props;
     const [toast, setToast] = useState({show: false, severity: "", text: ""})
 
-
     return (
       <>
         <Grid container spacing={4} className={classes.blogsWrapper} id="blog">
