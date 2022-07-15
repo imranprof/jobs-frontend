ecf7a4e24d198702a58b87b7e9396e42dcb51c9c
adittorehan <adittorehanshaju@gmail.com>
2022-07-15 17:52:41 +0600

    refactor: delete operations file in portfolio section

--- src/store/actions/portfolioActions.js
+++ src/store/actions/portfolioActions.js
@@ -1,4 +1,8 @@
+import axios from "axios";
+
 import {UPDATE_PORTFOLIO, REMOVE_PORTFOLIO, GET_PORTFOLIOS} from "../actionTypes/portfolioTypes";
+
+const profileURL = process.env.NEXT_PUBLIC_PROFILE_URL;
 
 export const updatePortfolio = (portfolios) => {
   return {
@@ -7,16 +11,50 @@ export const updatePortfolio = (portfolios) => {
   }
 }
 
-export const removePortfolio = (portfolios) => {
+export const removePortfolio = (portfolios, allCategories) => {
   return {
     type: REMOVE_PORTFOLIO,
-    payload: portfolios
+    payload: {
+      allPortfolios: portfolios,
+      allCategories: allCategories
+    }
   }
 }
 
-export const getPortfolios = (portfolios) => {
+export const removePortfolioAction = (portfolioID) => {
+  return (dispatch) => {
+    axios.patch(profileURL, {
+      "user": {
+        "projects_attributes": [
+          {
+            "id": portfolioID,
+            "_destroy": true
+          }
+        ]
+      }
+    }).then(res => dispatch(removePortfolio(res.data.portfolio_data.projects, res.data.all_categories)))
+      .catch(err => err.response)
+  }
+}
+
+export const getPortfolios = (portfolios, allCategories) => {
   return {
     type: GET_PORTFOLIOS,
-    payload: portfolios
+    payload: {
+      allPortfolios: portfolios,
+      allCategories: allCategories
+    }
+  }
+}
+
+export const getPortfoliosAction = (values) => {
+  const {id} = values
+  return (dispatch) => {
+    axios.get(profileURL, {
+      params: {
+        user_id: id
+      }
+    }).then(res => dispatch(getPortfolios(res.data.portfolio_data.projects, res.data.all_categories)))
+      .catch(err => err.response);
   }
 }
--- src/views/Profile/Portfolio/index.js
+++ src/views/Profile/Portfolio/index.js
@@ -1,5 +1,5 @@
 import {useState, useEffect} from "react";
-import {connect} from "react-redux";
+import {connect, useDispatch} from "react-redux";
 
 import {Grid} from "@material-ui/core";
 import {useTheme} from "@material-ui/core/styles";
@@ -7,8 +7,7 @@ import {useTheme} from "@material-ui/core/styles";
 import CustomCard from "../../../lib/profile/card/card";
 import {PortfolioStyle} from "./style";
 import CustomSnackbar from "../../../lib/customSnackbar";
-import {getPortfolios} from "../../../store/actions/portfolioActions"
-import {getPortfoliosData} from "./operations";
+import {getPortfolios, getPortfoliosAction} from "../../../store/actions/portfolioActions"
 
 
 const Portfolio = (props) => {
@@ -16,18 +15,11 @@ const Portfolio = (props) => {
   const classes = PortfolioStyle(theme);
   const {portfolios, userID} = props;
   const [toast, setToast] = useState({show: false, severity: "", text: ""})
+  let dispatch = useDispatch()
 
   useEffect(
     () => {
-      userID && getPortfoliosData({id: userID}).then(res => {
-          props.getPortfolios({
-            allPortfolios: res.portfolio_data.projects,
-            allCategories: res.all_categories
-          });
-        }
-      ).catch(
-        err => err
-      )
+      userID && dispatch(getPortfoliosAction({id: userID}))
     }, []
   )
 
