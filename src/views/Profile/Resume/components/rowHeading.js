256bf5639b2455d169b33f951eaecbf7af73be95
imranprof <imran.ent320@gmail.com>
2022-07-20 18:07:36 +0600

    feat: update headline of education and experience in resume section by backend data

--- src/views/Profile/Resume/components/rowHeading.js
+++ src/views/Profile/Resume/components/rowHeading.js
@@ -1,17 +1,36 @@
+import moment from "moment";
+
 import {useTheme} from "@material-ui/core/styles";
 
 import {ResumeStyle} from "../style";
 
-const RowHeading = ({cardType}) => {
-  const theme = useTheme();
-  const resumeWrapper = ResumeStyle(theme).resumeWrapper;
+const RowHeading = ({cardType, cardData}) => {
+    const theme = useTheme();
+    const resumeWrapper = ResumeStyle(theme).resumeWrapper;
+    const isSkillCard = cardType === "skills";
 
-  return (
-    <div className={`${resumeWrapper}__nav-content__row__heading`}>
-      <span>{"2007 - 2010"}</span>
-      <h4>{cardType} Quality</h4>
-    </div>
-  );
+    let startDate = 6000, endDate = -1;
+    if(!isSkillCard) {
+        for(let i = 0; i < cardData.length; i++){
+            let tempSDate = parseInt(moment(cardData[i].start_date).format('YYYY'), 10);
+            let tempEDate = parseInt(moment(cardData[i].end_date).format('YYYY'), 10);
+
+            if(startDate > tempSDate){
+                startDate = tempSDate;
+            }
+            if(endDate < tempEDate){
+                endDate = tempEDate;
+            }
+
+        }
+    }
+
+    return (
+        <div className={`${resumeWrapper}__nav-content__row__heading`}>
+            {!isSkillCard && <span>{startDate} - {endDate}</span>}
+            <h4>{cardType} Quality</h4>
+        </div>
+    );
 }
 
 export default RowHeading;
