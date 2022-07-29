ef765de6e44da572aca0ef9f89391a5dc006824b
imranprof <imran.ent320@gmail.com>
2022-07-29 20:00:13 +0600

    feat: use formik for contact form

--- src/views/Profile/Contact/components/contactForm.js
+++ src/views/Profile/Contact/components/contactForm.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react';
+import {connect, useDispatch} from "react-redux";
 
 import {Card, Grid} from "@material-ui/core";
 import Icon from "@material-ui/core/Icon";
@@ -6,8 +7,12 @@ import Icon from "@material-ui/core/Icon";
 import FontAwesomeIcons from "../../../../../styles/FontAwesomeIcons";
 import {FormErrorsData} from "../../../../../API/elements/profile/formErrorsData";
 import InputWrapper from "./inputWrapper";
+import {useFormik} from "formik";
+import {sendMessageAction} from "../../../../store/actions/contactActions";
 
-const ContactForm = ({classes}) => {
+const ContactForm = (props) => {
+  const {classes, userID} = props;
+  const dispatch = useDispatch();
   const initialInputValues = {
     name: "",
     phone: "",
@@ -21,53 +26,26 @@ const ContactForm = ({classes}) => {
   const [isSubmit, setIsSubmit] = useState(false)
   const [isSubmitted, setIsSubmitted] = useState(false)
 
-  const inputChangeHandler = (event) => {
-    const {name, value} = event.target
-    setFormValues({ ...formValues, [name]: value })
-  }
-
-  const submitFormHandler = (event) => {
-    event.preventDefault();
-    setFormErrors(validate())
-    setIsSubmit(true)
-  }
-
-  useEffect(() => {
-    if (Object.keys(formErrors).length === 0 && isSubmit) {
-      setFormValues(initialInputValues);
-      setIsSubmitted(true)
-      setTimeout(() => {
-        setIsSubmitted(false)
-      }, 2500)
+  const contactHandler = useFormik({
+    initialValues: initialInputValues,
+    onSubmit: values => {
+      dispatch(sendMessageAction({name: values.name, phone: values.phone, email: values.email, subject: values.subject, message: values.message, userID: userID, messengerId: null}))
+      contactHandler.resetForm()
     }
-  }, [formErrors])
+  })
 
-  const validate = () => {
-    const errors = {};
-    const regex = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}');
-    Object.entries(FormErrorsData).map(([key,value]) => {
-      let fieldValue = formValues[key]?.trim();
-      if(!fieldValue) {
-        errors[key] = value["required"]
-      } else if(key === "email" && !regex.test(fieldValue)){
-        errors[key] = value["regex"]
-      }
-    })
-    return errors;
-  }
 
   return (
     <Grid item md={7}>
       <Card className={`${classes}__contact-form`}>
-        <form onSubmit={submitFormHandler} >
           <Grid container spacing={4}>
-            <InputWrapper type="input" extraSmall={12} small={6} classes={classes} labelFor="name" labelName="Your Name" formValues={formValues.name} changeHandler={inputChangeHandler} />
-            <InputWrapper type="input" extraSmall={12} small={6} classes={classes} labelFor="phone" labelName="Phone Number" formValues={formValues.phone} changeHandler={inputChangeHandler} />
-            <InputWrapper type="input" extraSmall={12} small={12} classes={classes} labelFor="email" labelName="Email" formValues={formValues.email} changeHandler={inputChangeHandler} />
-            <InputWrapper type="input" extraSmall={12} small={12} classes={classes} labelFor="subject" labelName="Subject" formValues={formValues.subject} changeHandler={inputChangeHandler} />
-            <InputWrapper type="textarea" extraSmall={12} small={12} classes={classes} labelFor="message" labelName="Your Message" formValues={formValues.message} changeHandler={inputChangeHandler} />
+            <InputWrapper type="input" extraSmall={12} small={6} classes={classes} labelFor="name" labelName="Your Name"  formValues={contactHandler.values.name} changeHandler={contactHandler.handleChange}/>
+            <InputWrapper type="input" extraSmall={12} small={6} classes={classes} labelFor="phone" labelName="Phone Number"  formValues={contactHandler.values.phone} changeHandler={contactHandler.handleChange}/>
+            <InputWrapper type="input" extraSmall={12} small={12} classes={classes} labelFor="email" labelName="Email" formValues={contactHandler.values.email} changeHandler={contactHandler.handleChange}/>
+            <InputWrapper type="input" extraSmall={12} small={12} classes={classes} labelFor="subject" labelName="Subject" formValues={contactHandler.values.subject} changeHandler={contactHandler.handleChange}/>
+            <InputWrapper type="textarea" extraSmall={12} small={12} classes={classes} labelFor="message" labelName="Your Message" formValues={contactHandler.values.message} changeHandler={contactHandler.handleChange}/>
             <Grid item xs={12} className={`${classes}__contact-form__button-wrapper`}>
-              <button type='submit' className={`${classes}__send-message-btn`}>
+              <button type="submit" className={`${classes}__send-message-btn`} onClick={contactHandler.handleSubmit}>
                 <span className={`${classes}__send-message-btn__text`}>Send Message</span>
                 <Icon className={`${classes}__send-message-btn__icon ${FontAwesomeIcons.arrowRight}`} />
               </button>
@@ -83,10 +61,15 @@ const ContactForm = ({classes}) => {
               </div>
             )}
           </Grid>
-        </form>
       </Card>
     </Grid>
   );
 }
 
-export default ContactForm;
+const mapStateToProps = (state) => {
+  return{
+    userID: state.auth.userID
+  }
+}
+
+export default connect(mapStateToProps)(ContactForm);
