6f2e2de557cd8cb0d5fb536bf0ae6802dc50bcc3
imranprof <imran.ent320@gmail.com>
2022-08-18 18:47:07 +0600

    refactor: remove unused code

--- src/store/reducers/jobReducers.js
+++ src/store/reducers/jobReducers.js
@@ -1,8 +1,3 @@
-import {ADD_JOB} from "../actionTypes/jobTypes";
-
-const initialState = {
-  allJobs: []
-}
 
 export const jobReducers = (state = initialState, action) => {
   switch (action.type) {
