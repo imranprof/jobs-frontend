9c058c92e747035a7537d0696998f25ff492f8db
imranprof <imran.ent320@gmail.com>
2022-08-24 22:33:51 +0600

    feat: add infinite scroll in employer job

--- src/views/Profile/Jobs/Employer/myJobs.js
+++ src/views/Profile/Jobs/Employer/myJobs.js
@@ -1,23 +1,50 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
+import InfiniteScroll from "react-infinite-scroll-component";
 import {useDispatch} from "react-redux";
 import {connect} from "react-redux";
 
 import Job from "../../../Job";
 import {getIndividualJobs} from "../../../../store/actions/jobAction";
 import CustomLoader from "../../../../lib/customLoader";
+import EndMessage from "../../../../lib/endMessage";
 
 const MyJobs = (props) => {
   const dispatch = useDispatch()
   const {jobList, initialLoader} = props
+  const [hasMore, setHasMore] = useState(true);
+  const [jobs, setJobs] = useState([]);
 
-  useEffect(()=> {
+  useEffect(() => {
+    setJobs(jobList.slice(0, jobs.length + 8))
     dispatch(getIndividualJobs())
-  },[])
+  }, [])
+
+  if ((jobList.length > jobs.length && hasMore === false) || (jobs.length === 0 && jobList.length > jobs.length)) {
+    setJobs(jobList.slice(0, jobs.length + 8))
+  }
+
+  const fetchMoreData = () => {
+    if (jobs.length === jobList.length && jobs.length !== 0) {
+      setHasMore(false)
+    } else if (jobList.length !== 0) {
+      setTimeout(() => {
+        setJobs(jobList.slice(0, jobs.length + 8))
+      }, 1000)
+    }
+  }
 
   return (
     <div>
       {initialLoader && <CustomLoader/>}
-      {jobList.map((job) => <Job key={job.id} job={job}/>)}
+      <InfiniteScroll
+        dataLength={jobs.length}
+        next={fetchMoreData}
+        hasMore={hasMore}
+        loader={jobList.length===0 ? (<EndMessage title="Yay! You have seen it all"/> ) : (<CustomLoader/>)}
+        endMessage={<EndMessage title="Yay! You have seen it all"/>}
+      >
+        {jobs.map((job) => <Job key={job.id} job={job} />)}
+      </InfiniteScroll>
     </div>
   );
 };
