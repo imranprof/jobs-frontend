f8781e55c50012b5540cb1cba0658825b50c0abf
imranprof <imran.ent320@gmail.com>
2022-08-24 16:32:43 +0600

    feat: add my job list in employer profile

--- src/store/actions/jobAction.js
+++ src/store/actions/jobAction.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+import {SET_JOBS} from "../actionTypes/jobsTypes";
 
 const jobPostUrl = process.env.NEXT_PUBLIC_JOBS_URL
-
+const jobsUrl = process.env.NEXT_PUBLIC_My_JOB_URL
 
 export const addJobAction = (job) => {
   const {title, description, location, skills} = job;
@@ -17,6 +18,23 @@ export const addJobAction = (job) => {
   return async (dispatch) => {
     const response = axios.post(jobPostUrl, data)
       .then(res => res)
+      .catch(err => err.response)
+    return (response);
+  }
+}
+
+export const setIndividualJobs = (jobs) => {
+  return {
+    type: SET_JOBS,
+    payload: jobs
+  }
+}
+
+export const getIndividualJobs = () => {
+
+  return async (dispatch) => {
+    const response = axios.post(jobsUrl)
+      .then(res => dispatch(setIndividualJobs(res.data.jobs)))
       .catch(err => err.response)
     return (response);
   }
--- src/views/Profile/Jobs/Employer/myJobs.js
+++ src/views/Profile/Jobs/Employer/myJobs.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {useDispatch} from "react-redux";
+import {connect} from "react-redux";
 
-const MyJobs = () => {
+import Job from "../../../Job";
+import {getIndividualJobs} from "../../../../store/actions/jobAction";
+import CustomLoader from "../../../../lib/customLoader";
+
+const MyJobs = (props) => {
+  const dispatch = useDispatch()
+  const {jobList, initialLoader} = props
+
+  useEffect(()=> {
+    dispatch(getIndividualJobs())
+  },[])
+
   return (
     <div>
-      <h1>My job list</h1>
+      {initialLoader && <CustomLoader/>}
+      {jobList.map((job) => <Job key={job.id} job={job}/>)}
     </div>
   );
 };
 
-export default MyJobs;
+const mapStateToProps = (state) => {
+  return {
+    jobList: state.allJobs.individualJobs,
+    initialLoader: state.allJobs.initialLoader
+  }
+}
+
+export default connect(mapStateToProps)(MyJobs);
