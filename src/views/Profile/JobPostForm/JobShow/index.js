bc1ec2232caa3c5f7ba8ab0aeabd3879b0c91834
imranprof <imran.ent320@gmail.com>
2022-08-26 18:56:13 +0600

    feat: show applicant list in job details page for employer my jobs

--- src/views/Profile/JobPostForm/JobShow/index.js
+++ src/views/Profile/JobPostForm/JobShow/index.js
@@ -9,6 +9,9 @@ import {connect} from "react-redux";
 import CustomSnackbar from "../../../../lib/customSnackbar";
 import {getIndividualJobs, jobApplyAction} from "../../../../store/actions/jobAction";
 import {getRole} from "../../../../auth/operations";
+import {Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@material-ui/core";
+import Paper from '@material-ui/core/Paper'
+import Link from "next/link";
 
 const JobShow = (props) => {
   const theme = useTheme();
@@ -23,7 +26,15 @@ const JobShow = (props) => {
     await dispatch(getIndividualJobs())
   }, [])
 
-  let btnTitle = 'Apply', isId, isDisabled=false;
+  const hasApplicantsKey = data.hasOwnProperty('applicants')
+  let hasApplicants = false;
+  if(hasApplicantsKey){
+    if(data.applicants.length !== 0){
+      hasApplicants = true;
+    }
+  }
+
+  let btnTitle = 'Apply', isId, isDisabled = false;
   const handleClick = async () => {
     const response = await dispatch(jobApplyAction(id));
     if (response && response.status === 200) {
@@ -69,7 +80,7 @@ const JobShow = (props) => {
         <h3 className={`${classes.jobShowWrapper}__content-header`}>Client location</h3>
         <p className={`${classes.jobShowWrapper}__location`}>{location}</p>
       </div>
-      {role === 'employee' && <div>
+      {role === 'employee' ? (<div>
         <Divider className={`${classes.jobShowWrapper}__divider`}/>
         <span onClick={handleClick}>
           <Button
@@ -90,7 +101,38 @@ const JobShow = (props) => {
           Cancel
           </Button>
       </span>
-      </div>}
+      </div>) : (
+          hasApplicantsKey && hasApplicants && <>
+            <Divider className={`${classes.jobShowWrapper}__divider`}/>
+            <h3 className={`${classes.jobShowWrapper}__content-header`}>
+              Applicant List
+            </h3>
+            <TableContainer component={Paper}>
+              <Table aria-label={"customized table"}>
+                <TableHead>
+                  <TableRow>
+                    <TableCell style={{backgroundColor:"#000000"}}>Name</TableCell>
+                    <TableCell style={{backgroundColor:"#000000"}}>Profile</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {data.applicants.map((applicant)=> {
+                    const fullName = applicant.profile_slug.split("-")
+                    return (
+                      <TableRow key={applicant.profile_slug}>
+                        <TableCell style={{backgroundColor: "#242424"}}>{fullName[0]} {fullName[1]}</TableCell>
+                        <TableCell style={{backgroundColor: "#242424"}}><Link href={`${applicant.profile_slug}`} target="_blank"><a target="_blank" style={{color:"#686de0", textDecoration: "none"}}>More</a></Link></TableCell>
+                      </TableRow>
+                      )
+                  })
+
+                  }
+                </TableBody>
+              </Table>
+            </TableContainer>
+            </>
+        )
+      }
       {toast.show &&
       <CustomSnackbar
         toast={toast}
