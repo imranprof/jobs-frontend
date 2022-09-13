6265b9afa5d16895e7a85a56fa060aebb33599e8
imranprof <imran.ent320@gmail.com>
2022-09-13 22:09:38 +0600

    style: modify some style on chats.

--- src/views/Message/showMessage.js
+++ src/views/Message/showMessage.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import {Paper} from "@material-ui/core";
 
 const ShowMessage = (props) => {
   const {body,logged_in_user_id, sender_id} = props.data
@@ -8,17 +7,36 @@ const ShowMessage = (props) => {
   return (
     <>
       {sender_id === logged_in_user_id ? (
-          <div style={{display: "flex", justifyContent: "flex-end"}}>
-            <Paper style={{maxWidth: "70%", minWidth:"30%", marginBottom: "10px", padding: "10px", marginRight:"10px", backgroundColor:"#0084FF", borderRadius:"20px"}}>
+          <div style={{display: "flex", justifyContent: "flex-end", marginTop: "5px"}}>
+            <div style={{
+              display: "flex",
+              justifyContent: "center",
+              maxWidth: "70%",
+              minWidth:"10%",
+              marginBottom: "5px",
+              padding: "10px",
+              marginRight:"10px",
+              backgroundColor:"#0084FF",
+              borderRadius:"20px"
+            }}>
               <span style={{whiteSpace: "pre-wrap", margin: 0}}>{body}</span>
-            </Paper>
+            </div>
           </div>
         ) :
         (
-          <div style={{display: "flex", justifyContent: "flex-start"}}>
-            <Paper style={{maxWidth: "70%", marginBottom: "10px", padding: "10px", marginLeft: "10px", borderRadius:"20px"}}>
+          <div style={{display: "flex", justifyContent: "flex-start", marginTop: "5px"}}>
+            <div style={{
+              display: "flex",
+              justifyContent: "center",
+              maxWidth: "70%",
+              minWidth:"10%",
+              marginBottom: "5px",
+              padding: "10px",
+              backgroundColor: "#3E4042",
+              marginLeft: "10px",
+              borderRadius:"20px"}}>
               <span style={{whiteSpace: "pre-wrap", margin: 0}}>{body}</span>
-            </Paper>
+            </div>
           </div>
         )
 
