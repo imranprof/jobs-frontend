29370c5c0ef1402287cd00179a5d6bec130a902a
imranprof <imran.ent320@gmail.com>
2022-09-14 19:15:47 +0600

    feat: add selected user avatar and name in message section

--- src/pages/messages/index.js
+++ src/pages/messages/index.js
@@ -1,10 +1,10 @@
 import React, {useEffect, useState} from 'react';
 import withLayout from "../../views/Layout";
-import Divider from "@material-ui/core/Divider";
+import Divider from '@material-ui/core/Divider';
 import Message from "../../views/Message";
 import {useTheme} from "@material-ui/core/styles";
 import {MessagesStyle} from "./style";
-import {Grid, TextField} from "@material-ui/core";
+import {Avatar, Grid, TextField} from "@material-ui/core";
 import {getAllParentMessage, getPrivateConversations, sendMessageAction} from "../../store/actions/messageAction";
 import {useDispatch} from "react-redux";
 import {connect} from "react-redux";
@@ -32,7 +32,6 @@ const MessageList = (props) => {
     return () => clearInterval(interval);
   }, [parent_id])
 
-
   const sendMessageHandler = (values) => {
     dispatch(sendMessageAction({body: values.body, recipient_id: recipient_id, parent_message_id: parent_id}))
     formik.resetForm()
@@ -43,10 +42,40 @@ const MessageList = (props) => {
     onSubmit: sendMessageHandler
   });
 
+  let select = false;
+  let clickedUserAvatar = '';
+  let clickedUserName = '';
+  if (conversations.length > 0) {
+    select = true;
+    let len = conversations.length
+    let clickedUser = conversations[len - 1];
+    const {logged_in_user_id, recipient_avatar, recipient_name, sender_avatar, sender_id, sender_name} = clickedUser
+
+    if (logged_in_user_id === sender_id) {
+      clickedUserName = recipient_name;
+      clickedUserAvatar = recipient_avatar;
+    } else {
+      clickedUserName = sender_name;
+      clickedUserAvatar = sender_avatar;
+    }
+  }
+
 
   return (
     <>
-      <h1>Messages</h1>
+      <div style={{display: "flex", justifyContent: "space-between"}}>
+        <h1>Messages</h1>
+        {
+          select && <div style={{display: "flex", alignItems: "center", width: "70%"}}>
+            <Avatar
+              src={clickedUserAvatar}
+              alt="recipient avatar"
+            />
+            <span style={{marginLeft: "15px"}}>{clickedUserName}</span>
+          </div>
+        }
+      </div>
+
       <Divider/>
       {initialLoader && <CustomLoader/>}
 
@@ -56,9 +85,15 @@ const MessageList = (props) => {
             {allMessage.map((message) => <Message key={message.id} data={message} openChat={openChat}
                                                   setOpenChat={setOpenChat}/>)}
           </div>
-          <Divider orientation="vertical" className={`${classes.messagesWrapper}__divider`}/>
+          <Divider orientation="vertical" flexItem className={`${classes.messagesWrapper}__divider`}/>
           <div style={{width: "70%", minHeight: "700px"}}>
-            <div style={{height: "80%", overflowY: "auto", display: "flex", flexDirection:"column-reverse", marginBottom: "15px"}}>
+            <div style={{
+              height: "80%",
+              overflowY: "auto",
+              display: "flex",
+              flexDirection: "column-reverse",
+              marginBottom: "15px"
+            }}>
               {(openChat) && conversations.map((message) => <ShowMessage key={message.id} data={message}/>)}
             </div>
             <div style={{height: "20%", marginBottom: "20px"}}>
