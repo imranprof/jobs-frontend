7eec1df2b72398e6652f7b757fc8bd3781fae529
imranprof <imran.ent320@gmail.com>
2022-09-19 20:23:57 +0600

    feat: add search type select option in search box

--- src/lib/searchBar/index.js
+++ src/lib/searchBar/index.js
@@ -8,12 +8,19 @@ import SearchIcon from "@material-ui/icons/Search";
 
 import {SearchBarStyle} from "./style";
 import {getSearchProfiles} from "../../store/actions/searchAction";
+import Select from "react-select";
+import FontAwesomeIcons from "../../../styles/FontAwesomeIcons";
+import {selectStyles} from './style'
 
 const SearchBar = () => {
   const theme = useTheme();
   const classes = SearchBarStyle(theme);
   const dispatch = useDispatch()
 
+  const options = [
+    {value: 'Jobs', label: 'Jobs', text: 'Apply to jobs posted by clients', icon: FontAwesomeIcons.briefcase},
+    {value: 'Talents', label: 'Talents', text: 'Hire professionals', icon: FontAwesomeIcons.users}
+  ]
   const formik = useFormik({
     initialValues: {searchValue: ''},
     onSubmit: values => {
@@ -32,6 +39,18 @@ const SearchBar = () => {
         className={`${classes.searchBarWrapper}__search`}
         endAdornment={
           <InputAdornment position="end">
+            <Select options={options}
+                    styles={selectStyles}
+                    getOptionLabel={e => (
+                      <div style={{display: 'flex', alignItems: 'center'}}>
+                        <i className={e.icon}/>
+                        <div style={{display: "flex", flexDirection: "column"}}>
+                          <span style={{marginLeft: 5}}>{e.label}</span>
+                          <span style={{marginLeft: 5, fontSize: 12}}>{e.text}</span>
+                        </div>
+                      </div>
+                    )}
+            />
             <button type="submit" className={`${classes.searchBarWrapper}__search__icon`}>
               <SearchIcon/>
             </button>
