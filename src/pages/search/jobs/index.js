47acce59105f348fef438e5c372eb907c0f6b85c
imranprof <imran.ent320@gmail.com>
2022-09-19 22:03:00 +0600

    feat: add job search and talent search routes

---
+++ src/pages/search/jobs/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import withLayout from "../../../views/Layout";
+
+const JobSearch = () => {
+  return (
+    <div>
+      This is job search page
+    </div>
+  );
+};
+
+export default withLayout(JobSearch,'');
---
+++ src/pages/search/talents/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import withLayout from "../../../views/Layout";
+
+const TalentSearch = () => {
+  return (
+    <div>
+      this is talent search
+    </div>
+  );
+};
+
+export default withLayout(TalentSearch, '');
