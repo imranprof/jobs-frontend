57deeb43d83d40a2bfcd7013275fa94ec63175c9
imranprof <imran.ent320@gmail.com>
2022-09-20 17:35:58 +0600

    feat: add loader in talent search

--- src/pages/search/talents/index.js
+++ src/pages/search/talents/index.js
@@ -6,16 +6,19 @@ import ProfileCard from "../../../views/Profiles/ProfileCard";
 import {useTheme} from "@material-ui/core/styles";
 import {TalentsSearchStyle} from "./style";
 import {NoSsr} from "@material-ui/core";
+import CustomLoader from "../../../lib/customLoader";
 
-const TalentSearch = () => {
+const TalentSearch = (props) => {
   const theme = useTheme();
   const classes = TalentsSearchStyle(theme);
+  const {initialLoader} = props
 
   return (
     <NoSsr>
       <h2 className={`${classes.talentsSearchWrapper}__search-title`}>
         Results for "{getSearchValue()}"
       </h2>
+      {initialLoader && <CustomLoader/>}
       <div className={classes.talentsSearchWrapper}>
         <ProfileCard/>
       </div>
@@ -26,6 +29,7 @@ const TalentSearch = () => {
 const mapStateToProps = (state) => {
   return {
     searchProfiles: state.allProfiles.searchProfiles,
+    initialLoader: state.allProfiles.initialLoader
   }
 }
 
