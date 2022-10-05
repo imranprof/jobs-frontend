e1fac9366a521d4468196793cb8a36a0269fcb11
imranprof <imran.ent320@gmail.com>
2022-10-05 20:45:21 +0600

    style: modify hire form style

--- src/views/JobSeeker/HireForm/index.js
+++ src/views/JobSeeker/HireForm/index.js
@@ -2,6 +2,8 @@ import React, {useState} from 'react';
 import {Divider, Paper, Radio, TextField} from "@material-ui/core";
 import EditButton from "../../../lib/editButton";
 import {useFormik} from "formik";
+import Icon from "@material-ui/core/Icon";
+import FontAwesomeIcons from "../../../../styles/FontAwesomeIcons";
 
 const JobSeekerHireForm = () => {
   const [payTypeEditMode, setPayTypeEditMode] = useState(true)
@@ -12,12 +14,12 @@ const JobSeekerHireForm = () => {
     setSetSelectedValue(e.target.value)
     formik.values.payTypeValue = selectedValue
   }
-  const handlePayTypeMode = () => {
-    setPayTypeEditMode(!payTypeEditMode)
-  }
-  const handleRateMode = () => {
-    setRateEditMode(!rateEditMode)
-  }
+  // const handlePayTypeMode = () => {
+  //   setPayTypeEditMode(!payTypeEditMode)
+  // }
+  // const handleRateMode = () => {
+  //   setRateEditMode(!rateEditMode)
+  // }
 
   const formik = useFormik({
     initialValues: {
@@ -34,54 +36,57 @@ const JobSeekerHireForm = () => {
         <Divider style={{marginBottom: 10}}/>
         <div style={{marginLeft: 20}}>
           <h3>Payment Option</h3>
-          {payTypeEditMode ? (
-            <div style={{display: "flex", alignItems: "center"}}>
-              <h4>Pay Type ?</h4>
-              <span onClick={handlePayTypeMode}>
-                <EditButton/>
-              </span>
+          <div style={{width: "50%", display: "flex"}}>
+            <div style={{display: "flex", border: "1px solid", padding: 5, borderRadius: 10, marginRight: 10}}>
+              <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+                <Icon className={FontAwesomeIcons.clock}/>
+                <h4>Pay By The Hour</h4>
+              </div>
+              <span>
+                  <Radio
+                    checked={selectedValue === 'payByHour'}
+                    color="primary"
+                    value="payByHour"
+                    onChange={handleTypeChange}
+                  />
+                </span>
             </div>
-          ) : (
-            <div>
-              <Radio
-                checked={selectedValue === 'payByHour'}
-                color="primary"
-                value="payByHour"
-                onChange={handleTypeChange}
-              />
-              <Radio
-                checked={selectedValue === 'payFixedPrice'}
-                color="primary"
-                value="payFixedPrice"
-                onChange={handleTypeChange}
-              />
+
+            <div style={{display: "flex", border: "1px solid", padding: 5, borderRadius: 10}}>
+              <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+                <Icon className={FontAwesomeIcons.dollar}/>
+                <h4>Pay A Fixed Price</h4>
+              </div>
+              <span>
+                  <Radio
+                    checked={selectedValue === 'payFixedPrice'}
+                    color="primary"
+                    value="payFixedPrice"
+                    onChange={handleTypeChange}
+                  />
+                </span>
             </div>
-          )}
+          </div>
+
           {selectedValue === "payByHour" ? (
             <h3>Pay By the Hour</h3>
           ) : (
             <h3>Pay a Fixed Price</h3>
           )}
-          {rateEditMode ? (
-            <div style={{display: "flex", alignItems: "center", paddingBottom: 20}}>
-              <h4>$price here</h4>
-              <span onClick={handleRateMode}><EditButton/></span>
-            </div>
-          ) : (
-            <div style={{paddingBottom: 20}}>
-              <TextField
-                type="number"
-                size="small"
-                variant="outlined"
-                label="$"
-                name="hireRate"
-                value={formik.values.hireRate}
-                onChange={formik.handleChange}
-              />
-            </div>
-          )
 
-          }
+          <div style={{paddingBottom: 20}}>
+            <h4>Hire Rate</h4>
+            <h4>${formik.values.hireRate}</h4>
+            <TextField
+              type="number"
+              size="small"
+              variant="outlined"
+              label="$"
+              name="hireRate"
+              value={formik.values.hireRate}
+              onChange={formik.handleChange}
+            />
+          </div>
 
         </div>
       </Paper>
