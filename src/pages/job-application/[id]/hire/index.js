2c506ef8ecd92b27efea163ef1980ffdd009cde0
imranprof <imran.ent320@gmail.com>
2022-10-10 19:01:30 +0600

    refactor: remove dead code from hire page

--- src/pages/job-application/[id]/hire/index.js
+++ src/pages/job-application/[id]/hire/index.js
@@ -4,7 +4,6 @@ import withLayout from "../../../../views/Layout";
 import JobSeekerHireForm from "../../../../views/JobSeeker/HireForm";
 import {NoSsr} from "@material-ui/core";
 import {useRouter} from "next/router";
-import JobSeekerDetails from "../../../../views/JobSeeker/HireForm/JobSeekerDetails/JobSeekerDetails";
 import {getJobApplication} from "../../../../store/actions/jobAction";
 import {useDispatch} from "react-redux";
 import {connect} from "react-redux";
@@ -15,8 +14,6 @@ const HireJobSeeker = (props) => {
   const dispatch = useDispatch()
 
   const {initialLoader, applicationDetails} = props
-  const {applicant_details} = applicationDetails
-
   const router = useRouter()
   const {id} = router.query;
   useEffect(() => {
@@ -27,7 +24,6 @@ const HireJobSeeker = (props) => {
     <NoSsr>
       {initialLoader ? <CustomLoader/> :
         <>
-          <JobSeekerDetails avatar={applicant_details.avatar} name={applicant_details.name} email={applicant_details.email}/>
           <JobSeekerHireForm applicationDetails={applicationDetails}/>
         </>}
     </NoSsr>
