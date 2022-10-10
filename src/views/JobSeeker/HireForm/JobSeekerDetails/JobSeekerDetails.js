3576e160c67485e869d3c45318a521a4a4b893b3
imranprof <imran.ent320@gmail.com>
2022-10-10 21:40:29 +0600

    refactor: remove dead code from hire page profile details

--- src/views/JobSeeker/HireForm/JobSeekerDetails/JobSeekerDetails.js
+++ src/views/JobSeeker/HireForm/JobSeekerDetails/JobSeekerDetails.js
@@ -1,13 +1,10 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import {Avatar, Paper} from "@material-ui/core";
-import Icon from "@material-ui/core/Icon";
-import FontAwesomeIcons from "../../../../../styles/FontAwesomeIcons";
-import {connect, useDispatch} from "react-redux";
 import {useTheme} from "@material-ui/core/styles";
 import {JobSeekerDetailsStyle} from "./style";
 
 const JobSeekerDetails = (props) => {
-  const {avatar, name, email} = props
+  const {avatar, name} = props
   const theme = useTheme();
   const classes = JobSeekerDetailsStyle(theme);
 
