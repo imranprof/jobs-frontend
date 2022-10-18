df48b80c55af26b0462267b2909f5bc24e2ce008
Akash Das <aku.das02@gmail.com>
2022-10-19 01:20:05 +0600

    fix: resolve BestMatchesJobs component rendering issue in Home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,6 @@
+import {connect} from "react-redux";
+
+import {NoSsr} from "@material-ui/core";
 import {useTheme} from "@material-ui/core/styles";
 
 import withLayout from "../views/Layout";
@@ -5,17 +8,29 @@ import {RootStyle} from "./style";
 import Banner from "../views/Home/Banner";
 import CustomDivider from "../lib/profile/divider/divider";
 import BestMatchesJobs from "../views/Home/BestMatchesJobs";
+import {getRole} from "../auth/operations";
 
-const Root = () => {
+const Root = (props) => {
   const theme = useTheme();
   const classes = RootStyle(theme);
+  const {isAuthenticated} = props
+  const role = getRole()
 
   return (
-    <div className={`${classes.rootWrapper}__home`}>
-      <Banner />
-      <CustomDivider />
-      <BestMatchesJobs />
-    </div>
+    <NoSsr>
+      <div className={`${classes.rootWrapper}__home`}>
+        <Banner />
+        <CustomDivider />
+        {(role && role === "employee" && isAuthenticated) && <BestMatchesJobs role={role} />}
+      </div>
+    </NoSsr>
   )
 }
-export default withLayout(Root);
+
+const mapStateToProps = (state) => {
+  return {
+    isAuthenticated: state.auth.isAuthenticated,
+  }
+}
+
+export default connect(mapStateToProps)(withLayout(Root));
