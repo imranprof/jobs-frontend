8b1ac7ebfc79509dafd65551fa3e3c95f144968f
Akash Das <aku.das02@gmail.com>
2022-11-07 21:01:19 +0600

    feat: add all contacts nav link in profiles sidebar

--- src/views/Profile/Header/components/sideBar.js
+++ src/views/Profile/Header/components/sideBar.js
@@ -13,62 +13,63 @@ import SocialLinks from "../../../../lib/profile/socialLinks";
 import ProfileShare from "../../../../lib/profile/profileshare/profileShare";
 
 const ProfileSideBar = (props) => {
-    const { profileInfo, classes } = props;
-    const {name, avatar} = profileInfo;
-    const [open, setOpen] = useState(false);
+  const {profileInfo, classes} = props;
+  const {name, avatar} = profileInfo;
+  const [open, setOpen] = useState(false);
 
-    return (
-        <>
-            <Hidden lgUp>
-                <IconButton
-                    className={`${classes.headerWrapper}__menu`}
-                    onClick={() => setOpen(true)}
-                    role="button"
-                    tabIndex={0}
-                    aria-label="opens the sidebar for small devices">
-                    <MenuIcon className={`${classes.headerWrapper}__menu__icon`}/>
-                </IconButton>
-            </Hidden>
+  return (
+    <>
+      <Hidden lgUp>
+        <IconButton
+          className={`${classes.headerWrapper}__menu`}
+          onClick={() => setOpen(true)}
+          role="button"
+          tabIndex={0}
+          aria-label="opens the sidebar for small devices">
+          <MenuIcon className={`${classes.headerWrapper}__menu__icon`}/>
+        </IconButton>
+      </Hidden>
 
-            <SwipeableDrawer
-                open={open} anchor="left"
-                onOpen={() => setOpen(true)}
-                onClose={() => setOpen(false)}
+      <SwipeableDrawer
+        open={open} anchor="left"
+        onOpen={() => setOpen(true)}
+        onClose={() => setOpen(false)}
+      >
+        <div className={`${classes.headerWrapper}__side-bar`}>
+          <div className={`${classes.headerWrapper}__side-bar__icons`}>
+            <ProfileInfo name={name} avatar={avatar} showName={false} classes={classes}/>
+            <IconButton
+              className={`${classes.headerWrapper}__close-icon`}
+              onClick={() => setOpen(false)}
             >
-                <div className={`${classes.headerWrapper}__side-bar`}>
-                    <div className={`${classes.headerWrapper}__side-bar__icons`}>
-                        <ProfileInfo name={name} avatar={avatar} showName={false} classes={classes}/>
-                        <IconButton
-                            className={`${classes.headerWrapper}__close-icon`}
-                            onClick={() => setOpen(false)}
-                        >
-                            <CloseIcon/>
-                        </IconButton>
-                    </div>
+              <CloseIcon/>
+            </IconButton>
+          </div>
 
-                    <p className={`${classes.headerWrapper}__profile__name ${classes.headerWrapper}__profile__name--drawer`}>
-                        {name}
-                    </p>
+          <p className={`${classes.headerWrapper}__profile__name ${classes.headerWrapper}__profile__name--drawer`}>
+            {name}
+          </p>
 
-                    <Divider/>
-                    <div className={`${classes.headerWrapper}__side-bar__links`}>
-                        <NavItems classes={classes} variant={"drawer"}/>
-                    </div>
-                    <Divider style={{marginBottom: "30px"}}/>
+          <Divider/>
+          <div className={`${classes.headerWrapper}__side-bar__links`}>
+            <NavItems classes={classes} variant={"drawer"}/>
+          </div>
+          <Divider style={{marginBottom: "30px"}}/>
 
-                    <SocialLinks/>
-                    <p className={`${classes.headerWrapper}__side-bar__share-bar__title`}>Share Me</p>
-                    <ProfileShare shareUrl={"http://rightcodes.org/"}  boxClass={`${classes.headerWrapper}__side-bar__share-bar`} iconClass={`${classes.headerWrapper}__side-bar__share-bar__icon`}/>
-                </div>
-            </SwipeableDrawer>
-        </>
-    );
+          <SocialLinks/>
+          <p className={`${classes.headerWrapper}__side-bar__share-bar__title`}>Share Me</p>
+          <ProfileShare shareUrl={"http://rightcodes.org/"} boxClass={`${classes.headerWrapper}__side-bar__share-bar`}
+                        iconClass={`${classes.headerWrapper}__side-bar__share-bar__icon`}/>
+        </div>
+      </SwipeableDrawer>
+    </>
+  );
 };
 
 const mapStateToProps = (state) => {
-    return {
-        profileInfo: state.topSection
-    }
+  return {
+    profileInfo: state.topSection
+  }
 }
 
 export default connect(mapStateToProps)(ProfileSideBar);
