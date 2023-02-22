420988c824e394b26ccd22639c4d647737ede4f3
Akash Das <aku.das02@gmail.com>
2023-02-22 19:08:38 +0600

    fix: disable redux-logger for production.

--- src/store/index.js
+++ src/store/index.js
@@ -1,19 +1,25 @@
 import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
-import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
 import { createWrapper } from "next-redux-wrapper";
 
 import rootReducer from './reducers'
 
 // initial states here
 const initialState = {};
+let middleware = [];
+if (process.env.NODE_ENV === 'development') {
+  middleware = [...middleware, thunk, logger];
+} else {
+  middleware = [...middleware, thunk];
+}
 
 // creating store
 export const store = createStore(
     rootReducer,
     initialState,
-    composeWithDevTools(applyMiddleware(logger, thunk)),
+    composeWithDevTools(applyMiddleware(...middleware)),
 );
 
 // assigning store to next wrapper
